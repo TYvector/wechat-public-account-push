cdc9e1485b6c94cb5b10f005e965ace7a4d28c79
TYvector <42628673+TYvector@users.noreply.github.com>
2023-04-23 16:11:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o96kG58Yn3pmif4oMO81AMfVoDFU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'HlIh62RJp8qsHj49uZmR0SXGbQTMXipT9vecGbkMU5s	',
+      useTemplateId: 'HlIh62RJp8qsHj49uZmR0SXGbQTMXipT9vecGbkMU5s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
